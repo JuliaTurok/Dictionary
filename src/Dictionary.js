ca47722a3356a71f7f0d01a6eb84890aaa98b9fc
JuliaTurok <107355921+JuliaTurok@users.noreply.github.com>
2022-10-05 16:21:31 +0300

    imported axios

---
+++ src/Dictionary.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function LoginForm() {
+  let [keyword, setKeyword] = useState("");
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function updateKeyword(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Keyword" onChange={updateKeyword} />
+
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}

334ece019e0cfa811f3614a24feffce3c8a33389
JuliaTurok <107355921+JuliaTurok@users.noreply.github.com>
2022-10-12 17:59:29 +0300

    added rel

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 export default function Phonetic(props){
-return (<div className ="Phonetic">
-    <a href={props.phonetic.audio} target="_blank">Listen</a>
+return (
+  <div className="Phonetic">
+    <a href={props.phonetic.audio} rel="noreferrer" target="_blank">
+      Listen
+    </a>
     <br />
     {props.phonetic.text}
-</div>)
+    <br />
+  </div>
+);
 }
